0bf657df53c619916fd94a905b3075b113e76c48
gianluca-vedovato <vedovatogianluca@gmail.com>
2024-10-13 08:16:45 +0200

    feat(backend): :sparkles: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(3000);
 }
 bootstrap();
